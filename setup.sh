cb03dd82e255c2fb34a7d50a5f99440f5fe078e2
vinhgiga <vinhgiga@gmail.com>
2025-01-01 12:55:02 +0700

    fix: copy hidden files

--- setup.sh
+++ setup.sh
@@ -9,22 +9,13 @@ function copyIt() {
   PATTERN="${PATTERN:1}" # Remove the leading "|"
 
   # Use cp and grep with the exclusion pattern.
-  cp -r $(ls | grep -v -E "(${PATTERN})") $HOME
+  cp -r $(ls -A | grep -v -E "(${PATTERN})") $HOME
 
   # Install zsh if it is not already installed
-  if ! command -v zsh &> /dev/null; then
+  if ! command -v zsh &>/dev/null; then
     echo "Zsh is not installed. Installing zsh..."
     bash "$HOME/scripts/install-zsh.sh"
   fi
-  # Autostart zsh if bash is the default shell
-  cat <<EOT >> $HOME/.bashrc
-
-if command -v zsh &> /dev/null; then
-  exec zsh
-else
-  exec "$HOME/.local/bin/zsh"
-fi
-EOT
 
   # # Define the files and directories to exclude
   # EXCLUDES=(
@@ -41,6 +32,16 @@ EOT
   # # Use tar to archive the directory excluding specific files, then extract to $HOME
   # tar -cf - $EXCLUDE_ARGS . | tar -xf - -C "$HOME"
 
+  # Autostart zsh if bash is the default shell
+  cat <<EOT >>$HOME/.bashrc
+
+if command -v zsh &> /dev/null; then
+  exec zsh
+else
+  exec "$HOME/.local/bin/zsh"
+fi
+EOT
+
   echo "Setup complete."
 }
 
@@ -48,13 +49,17 @@ if [[ -z "$1" && ("$1" == "--force" || "$1" == "-f") ]]; then
   copyIt
 else
   while true; do
-    read -p "This will overwrite files in your home directory. Are you sure? (y/n) " -n 1
-    echo ""
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
+    printf "This will overwrite files in your home directory. Are you sure? (y/n) "
+    read -n 1 answer
+    echo # Move to a new line after input
+    case "$answer" in
+    [Yy])
       copyIt
       break
-    elif [[ $REPLY =~ ^[Nn]$ ]]; then
+      ;;
+    [Nn])
       break
-    fi
+      ;;
+    esac
   done
 fi
